ebdb12164dabc79626a67259675ada439d08fcd4
Iiro Moilanen <t9moii00@students.oamk.fi>
2020-11-06 13:07:23 +0200

    First Commit

--- app/src/main/java/com/example/activate/HakuValikko.java
+++ app/src/main/java/com/example/activate/HakuValikko.java
@@ -1,4 +1,32 @@
 package com.example.activate;
 
-public class HakuValikko {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View;
+import android.widget.Button;
+
+public class HakuValikko extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hakuvalikko);
+
+        findViewById(R.id.button).setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        Button button;
+
+        if (view instanceof Button) {
+            button = (Button) view;
+            if (button.getId() == R.id.button) {
+                Intent intent = new Intent(this, HakutulosValikko.class);
+                startActivity(intent);
+            }
+        }
+    }
 }
